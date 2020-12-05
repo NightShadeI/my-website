5b0829c73d42d7c98ecd396a5ce27e37f308f148
NightShadeI <hontapner@gmail.com>
2020-12-05 16:10:23 +1100

    Further improvements to styling

--- src/components/home.js
+++ src/components/home.js
@@ -5,14 +5,15 @@ const Home = () => {
 
 	return (
 		<div className="home">
-			<video loop autoPlay muted id="stars">
-				<source src="/videos/stars.mp4" type="video/mp4"></source>
-      		</video>
+			<img src="/images/stars.jpg" id="stars" alt="stars"/>
 			<Route path="/home/introductions">
 				<div className = "messagebox">
-					<h1>Introductions</h1>
+					<h1>Welcome!</h1>
 					<Message>
-						<span>Hello there, and welcome to my website! This is a space where you can read about me (Thomas Tapner), my projects, my experience and the various skills that I have. I will continue to update this space over time as new knowledge comes to light.</span>
+						<span>Hello there, and welcome to my website! 😊 This is a space where you can read about me (Thomas Tapner), my projects, my experience and the various skills that I have. I will continue to update this space over time as new knowledge comes to light.</span>
+					</Message>
+					<Message>
+						<span>To navigate this website, you can navigate through the various sections on the left hand side. The top of the website contains sub-points for navigation, for more tailored information</span>
 					</Message>
 					<Message>
 						<span>This has been created using <a href="https://reactjs.org/">react</a> to design the user interface. The backend is built using express. All styling is done using regular CSS. All design is publically available on Github at <a href="https://github.com/NightShadeI/my-website">this</a> link.</span>				
